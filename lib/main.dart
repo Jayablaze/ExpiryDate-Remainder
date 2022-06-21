97d3df3be4bd664c4d4d8719da0fa7d65b2f6f9e
Jayachandran <jayachandran.kaliyamoorthi@mpura.com>
2022-06-21 12:31:05 +0530

    Book Api added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -28,11 +30,27 @@ class MyHomePage extends StatefulWidget {
 
 class _QrCodeState extends State<MyHomePage> {
   String _data = "";
-
+  String data1 = '';
+  final Uri _url = Uri.parse('https://WWW.google.com/search?q=');
   _scan() async {
     await FlutterBarcodeScanner.scanBarcode(
             "#000000", "Cancel", true, ScanMode.BARCODE)
         .then((value) => setState(() => _data = value));
+    _launchUrl(_data);
+  }
+
+  static final http.Client httpClient = http.Client();
+  void _launchUrl(value) async {
+    print("ghchgchg");
+    final data = await httpClient.get(
+      Uri.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:' + value),
+    );
+
+    print("sakjkdsajdasj" + data.body.toString());
+    setState(() {
+      data1 = data.body;
+    });
+    print("jzxnkjzxkjc" + data.reasonPhrase.toString());
   }
 
   @override
@@ -48,10 +66,14 @@ class _QrCodeState extends State<MyHomePage> {
                 child: Text("Scan Barcode"),
                 onPressed: () => _scan(),
               ),
-              Text(_data),
               Container(
                 child: Text(
                   _data,
+                ),
+              ),
+              Container(
+                child: Text(
+                  data1,
                 ),
               )
             ],
